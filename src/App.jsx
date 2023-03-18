d789912e242eb21972305d29d19a34c8c32c35b0
KuronoKazuki <kurono1215@gmail.com>
2023-03-19 03:05:06 +0900

    add:チェックボックス機能追加、人口推移データをPrefectureに統合

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,12 @@ import axios from "axios";
 import Prefecture from './Prefecture'
 
 function App() {
-  const [populationData, setPopulationData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await axios.get(
-          "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=40",
-          {
-            headers: {
-              "X-API-KEY": process.env.REACT_APP_API_KEY,
-            }
-          }
-        )
-        setPopulationData(result.data.result.data[0].data);
-      }catch(error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-
-  }, []);
-
   return (
     <div className="App">
       <header style={{ textAlign: "center" }}>
         <h1>日本の人口推移</h1>
       </header>
       <Prefecture />
-      <div>
-        {populationData.map((data) => (
-          <p key={data.year}>{`${data.year}年: ${data.value}人`}</p>
-        ))}
-      </div>
     </div>
   );
 }
--- src/Prefecture.jsx
+++ src/Prefecture.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+//都道府県を表示
 function Prefecture() {
-	const [prefectureData, setprefectureData] = useState([]);
+	//人口データと都道府県データを管理するステート
+	const [populationData, setPopulationData] = useState([]);
+	const [prefectureData, setPrefectureData] = useState([]);
   
+	//最初にページが読み込まれたときに都道府県データを取得する
 	useEffect(() => {
-	  const fetchData = async () => {
+	  const prefectureData = async () => {
 		try {
 		  const data = await axios.get(
 			"https://opendata.resas-portal.go.jp/api/v1/prefectures",
@@ -15,21 +19,73 @@ function Prefecture() {
 			  }
 			}
 		  )
-		  setprefectureData(data.data.result);
+		  setPrefectureData(data.data.result);
 		}catch(error) {
 		  console.error(error);
 		}
 	  };
-	  fetchData();
+	  prefectureData();
 	}, []);
+
+	//チェックボックスの状態を管理するステート
+	const [checkedValues, setCheckedValues] = useState([]);
+
+	//チェックボックスが変更されたときの処理
+	const handleChange = (event) => {
+		const { value, checked } = event.target;
+		if(checked){
+			//チェックされた都道府県を設定する
+			setCheckedValues([...checkedValues, value]);
+		}else{
+			//チェックが外れた都道府県を集計から外す
+			const newCheckedValues = checkedValues.filter((code) => code !== value);
+			setCheckedValues(newCheckedValues);
+		}
+	};
+			useEffect(() => {
+				const fetchData = async () => {
+					if (checkedValues.length > 0) {
+						try {
+							console.log(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=` + checkedValues);
+							const result = await axios.get(
+								`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=` + checkedValues,
+								{
+									headers: {
+										"X-API-KEY": process.env.REACT_APP_API_KEY,
+									},
+								}
+							);
+							console.log(result);
+							setPopulationData(result.data.result.data[0].data);
+						}catch(error) {
+							console.error(error);
+						}
+					}
+				};
+				fetchData();
+			}, [checkedValues]);
+
+
   
 	return (
 		<div>
 		  {prefectureData.map((data) => (
-			<p key={data.prefCode}>{`${data.prefName}`}</p>
+			<label>
+				<input
+					type="checkbox"
+					key={data.prefCode}
+					value={data.prefCode}
+					onChange={handleChange}
+				/>
+				{`${data.prefName}`}
+			</label>
 		  ))}
+			<div>
+        {populationData.map((data) => (
+          <p key={data.year}>{`${data.year}年: ${data.value}人`}</p>
+        ))}
+      </div>
 		</div>
 	);
   }
-  
   export default Prefecture;
