ed31a4e1885baccc50a63d0c2e6a346a8c17dc43
KuronoKazuki <kurono1215@gmail.com>
2023-03-19 19:01:33 +0900

    add:グラフを追加

--- src/Prefecture.jsx
+++ src/Prefecture.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 //都道府県を表示
 function Prefecture() {
@@ -27,28 +29,53 @@ function Prefecture() {
 	  prefectureData();
 	}, []);
 
+	//グラフデータの状態を管理するステート
+	const [series, setSeries] = useState([]);
+
 	//チェックボックスの状態を管理するステート
 	const [checkedValues, setCheckedValues] = useState([]);
 
 	//チェックボックスが変更されたときの処理
 	const handleChange = (event) => {
-		const { value, checked } = event.target;
+		const { value, checked, name } = event.target;
 		if(checked){
 			//チェックされた都道府県を設定する
-			setCheckedValues([...checkedValues, value]);
+			setCheckedValues([value, name]);
 		}else{
 			//チェックが外れた都道府県を集計から外す
-			const newCheckedValues = checkedValues.filter((code) => code !== value);
-			setCheckedValues(newCheckedValues);
+			setSeries(series.filter(elm => {
+				return elm.name != name;
+			}))
 		}
 	};
+
+	  //Highchartsの設定オプション
+		let years = populationData.map((item) => item.year);
+		const chartOptions = {
+			title: {
+				text: "都道府県別人口推移",
+			},
+			xAxis: {
+				title: {
+					text: "年度",
+				},
+				categories: years
+			},
+			yAxis: {
+				title: {
+					text: "人口数",
+				},
+			},
+			series: series
+		};
+
 			useEffect(() => {
 				const fetchData = async () => {
 					if (checkedValues.length > 0) {
 						try {
-							console.log(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=` + checkedValues);
+							console.log(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=` + checkedValues[0]);
 							const result = await axios.get(
-								`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=` + checkedValues,
+								`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=` + checkedValues[0],
 								{
 									headers: {
 										"X-API-KEY": process.env.REACT_APP_API_KEY,
@@ -57,6 +84,13 @@ function Prefecture() {
 							);
 							console.log(result);
 							setPopulationData(result.data.result.data[0].data);
+							const newPopulationData = result.data.result.data[0].data;
+							let populationValue = newPopulationData.map((item) => item.value);
+							setSeries([...series,{
+								name: checkedValues[1],
+								data: populationValue
+							}]);
+							console.log(series)
 						}catch(error) {
 							console.error(error);
 						}
@@ -75,11 +109,15 @@ function Prefecture() {
 					type="checkbox"
 					key={data.prefCode}
 					value={data.prefCode}
+					name={data.prefName}
 					onChange={handleChange}
 				/>
 				{`${data.prefName}`}
 			</label>
 		  ))}
+			<div>
+        <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+      </div>
 			<div>
         {populationData.map((data) => (
           <p key={data.year}>{`${data.year}年: ${data.value}人`}</p>
